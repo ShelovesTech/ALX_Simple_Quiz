bed5bdb8de6ebb1d7fc68a36b8bdadde47487b79
karanjaclaire <karanjaclaire2@gmail.com>
2024-08-04 23:36:54 +0300

    simple quiz

---
+++ quiz.js
@@ -0,0 +1,33 @@
+
+// DOM Manipiulation 
+
+// Function Declaration
+function checkAnswer() {
+  
+    // Identify the Correct Answer
+    let correctAnswer = '4';
+  
+    // Retrieve the User's Answer
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    
+    // User Feedback 
+  
+    let feedback = document.getElementById('feedback');
+  
+    // Compare the User's Answer with the Correct Answer
+  
+    if (userAnswer === correctAnswer) {
+     feedback.textContent = 'Correct! Well done.';
+     feedback.style.color = '#007bff';
+    } else {
+     feedback.textContent = "That's incorrect. Try again!";
+     feedback.style.color = 'orange';
+    }
+  
+  
+  } 
+  
+  // Add EventListener to the Submit Button
+  document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+  
+
